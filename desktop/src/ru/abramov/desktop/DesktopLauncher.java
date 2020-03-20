805cf26751c31b4e993294159a30184a5ac9aa1d
Iurii Abramov <abtank@outlook.com>
2020-03-21 02:01:30 +0300

    Initial commit

---
+++ desktop/src/ru/abramov/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.abramov.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.abramov.AbStarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AbStarGame(), config);
+	}
+}
