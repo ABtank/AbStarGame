419bee8017ba5e399f9638fb1ba8bf5676e5326b
Iurii Abramov <abtank@outlook.com>
2020-03-29 00:10:36 +0300

    framework games

--- core/src/ru/abramov/screen/MenuScreen.java
+++ core/src/ru/abramov/screen/MenuScreen.java
@@ -4,7 +4,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
-import ru.abramov.Background;
+
+import ru.abramov.exeption.GameExaption;
+import ru.abramov.math.Rect;
+import ru.abramov.sprites.Background;
 import ru.abramov.base.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
@@ -12,24 +15,37 @@ public class MenuScreen extends BaseScreen {
 
     private Vector2 pos;
     private Texture img;
+    private Texture bg;
     private Background background;
 
     @Override
     public void show() {
         super.show();
         img = new Texture("menuScreen.png");
-        background = new Background();
+        bg =new Texture("background.jpg");
+        try {
+            background = new Background(bg);
+        } catch (GameExaption e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
         pos = new Vector2();
     }
 
     @Override
-    public void render(float delta) {
-        update(delta);
+    public void render(float deltatime) {
+        update(deltatime);
         draw();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+    public void resize(Rect worldBounds) {
+        background.resize(worldBounds);
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        pos.set(touch);
         return false;
     }
 
@@ -39,17 +55,18 @@ public class MenuScreen extends BaseScreen {
     private void draw() {
         Gdx.gl.glClearColor(0.5f, 0.7f, 0.8f, 0.5f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        spriteBatch.begin();
-        background.render(spriteBatch);
-        spriteBatch.draw(img, pos.x, pos.y);
-        spriteBatch.end();
+        batch.begin();
+        background.drow(batch);
+        batch.draw(img, pos.x, pos.y, 0.2f, 0.2f);
+        batch.end();
     }
 
 
     @Override
     public void dispose() {
-        spriteBatch.dispose();
+        batch.dispose();
         img.dispose();
+        bg.dispose();
         super.dispose();
     }
 }
